319fe970e706b3315b66fa499023f178ca9b6f22
Erik Lopez <eriklo@zillowgroup.com>
2023-11-09 23:52:13 -0700

    feat: Pass values and store in state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-import { Flex, Button } from "@radix-ui/themes";
+"use client";
+
+import { useState } from "react";
+import { Flex, Button, Text } from "@radix-ui/themes";
 import NewGame from "@/components/NewGame";
 
+export interface StartGameProps {
+  startingLife: number;
+  players: string[];
+}
+
 function Page() {
+  const [started, setStarted] = useState<boolean>(false);
+  const [life, setLife] = useState<number>();
+  const [playerNames, setPlayerNames] = useState<string[]>();
+
+  const startGame = ({ startingLife, players }: StartGameProps) => {
+    console.log(startingLife, players);
+    setLife(startingLife);
+    setPlayerNames(players);
+    setStarted(true);
+  };
+
   return (
     <Flex align="center" justify="center">
       <Flex
@@ -9,11 +28,21 @@ function Page() {
         gap="4"
         style={{ width: "180px", height: "fit-content" }}
       >
-        <NewGame />
-        <Button size="3" disabled>
-          Continue
-        </Button>
-        <Button size="3">About</Button>
+        {started && (
+          <Text size="3">
+            {life}
+            {playerNames}
+          </Text>
+        )}
+        {!started && (
+          <>
+            <NewGame startGame={startGame} />
+            <Button size="3" disabled>
+              Continue
+            </Button>
+            <Button size="3">About</Button>
+          </>
+        )}
       </Flex>
     </Flex>
   );
