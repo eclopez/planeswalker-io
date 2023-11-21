cd936046af5aa2d82cd81cf8e8f0443c113a0d87
eclopez <erik@eriklopez.net>
2023-11-21 08:32:33 -0700

    feat: Add useDebounce to Player

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Card, Flex, Text, Button } from "@radix-ui/themes";
 import { PlusIcon, MinusIcon } from "@radix-ui/react-icons";
+import useDebounce from "@/hooks/useDebounce";
 import useLocalStorage from "@/hooks/useLocalStorage";
 
 interface PlayerProps {
@@ -13,26 +14,33 @@ interface PlayerProps {
 function Player({ gameId, playerId, name, life }: PlayerProps) {
   const [currentLife, setCurrentLife] = useState<number>(life);
   const { updateGame } = useLocalStorage();
+  const debouncedLifeTotal = useDebounce<number>(currentLife, 700);
 
-  // TODO: use a debounce here
-  const updateLife = (updatedLife: number) => {
-    setCurrentLife((l) => {
-      let newLife = l + updatedLife;
-      updateGame(gameId, playerId, newLife);
-      return newLife;
-    });
+  const handleLifeChange = (delta: number) => {
+    setCurrentLife((life) => life + delta);
   };
+
+  useEffect(() => {
+    updateGame(gameId, playerId, currentLife);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedLifeTotal]);
 
   return (
     <Card>
       <Flex gap="1" justify={"between"}>
         <Text size="7">{name}</Text>
         <Flex>
-          <Button onClick={() => updateLife(1)}>
+          <Button
+            data-testid="incrementLife"
+            onClick={() => handleLifeChange(1)}
+          >
             <PlusIcon />
           </Button>
           <Text size="7">{currentLife}</Text>
-          <Button onClick={() => updateLife(-1)}>
+          <Button
+            data-testid="decrementLife"
+            onClick={() => handleLifeChange(-1)}
+          >
             <MinusIcon />
           </Button>
         </Flex>
--- src/components/__tests__/Players.test.tsx
+++ src/components/__tests__/Players.test.tsx
@@ -1,8 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Player from "@/components/Player";
 
-jest.mock("next/navigation", () => ({ useRouter: jest.fn }));
+jest.mock("../../hooks/useLocalStorage", () => ({
+  __esModule: true,
+  default: () => {
+    return { updateGame: jest.fn };
+  },
+}));
 
 describe("Player", () => {
   it("should render the Player component with the correct props", () => {
@@ -12,5 +17,33 @@ describe("Player", () => {
 
     expect(screen.getByText("Erik")).toBeInTheDocument();
     expect(screen.getByText("32")).toBeInTheDocument();
+  });
+
+  it("should increment the Player's life when the increment life button is pressed", () => {
+    render(
+      <Player gameId={"plw-24154324532"} playerId={0} name={"Erik"} life={32} />
+    );
+
+    expect(screen.getByText("Erik")).toBeInTheDocument();
+    expect(screen.getByText("32")).toBeInTheDocument();
+
+    const incrementLifeButton = screen.getByTestId("incrementLife");
+    fireEvent.click(incrementLifeButton);
+
+    expect(screen.getByText("33")).toBeInTheDocument();
+  });
+
+  it("should decrement the Player's life when the increment life button is pressed", () => {
+    render(
+      <Player gameId={"plw-24154324532"} playerId={0} name={"Erik"} life={32} />
+    );
+
+    expect(screen.getByText("Erik")).toBeInTheDocument();
+    expect(screen.getByText("32")).toBeInTheDocument();
+
+    const decrementLifeButton = screen.getByTestId("decrementLife");
+    fireEvent.click(decrementLifeButton);
+
+    expect(screen.getByText("31")).toBeInTheDocument();
   });
 });
