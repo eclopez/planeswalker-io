a97af2160776be9f94ad7555171a72d83773edbe
eclopez <erik@eriklopez.net>
2023-11-22 21:19:22 -0700

    test: Make sure server is running before running tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
       testMatch: ["**/?(*.)spec.+(js|ts)"],
     },
   ],
+  webServer: {
+    command: "npm run dev",
+    url: "http://127.0.0.1:3000",
+    reuseExistingServer: !process.env.CI,
+  },
 });
