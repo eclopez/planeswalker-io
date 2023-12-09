870179e505faef79d665b491d99d8d1298545d71
eclopez <erik@eriklopez.net>
2023-12-09 14:28:22 -0700

    feat: Add replacer and reviver functions to store and retrieve commander damage map

--- src/helpers/localStorageHelper.ts
+++ src/helpers/localStorageHelper.ts
@@ -5,13 +5,36 @@ import {
   PlayerCounterTypesRecord,
 } from "@/types/GameTypes";
 
+const mapReplacer = (_: string, v: any) => {
+  if (v instanceof Map) {
+    return {
+      dataType: "Map",
+      value: [...v],
+    };
+  } else {
+    return v;
+  }
+};
+
+const mapReviver = (_: string, v: any) => {
+  if (typeof v === "object" && v !== null) {
+    if (v.dataType === "Map") {
+      return new Map(v.value);
+    }
+  }
+  return v;
+};
+
 const saveGame = (game: GameTypes) => {
   const gameId = Object.keys(game)[0];
-  localStorage.setItem(gameId, JSON.stringify(game[gameId]));
+  localStorage.setItem(gameId, JSON.stringify(game[gameId], mapReplacer));
 };
 
 const loadGame = (gameId: string): GameTypes => {
-  return { [gameId]: JSON.parse(localStorage.getItem(gameId)) };
+  const game = {
+    [gameId]: JSON.parse(localStorage.getItem(gameId), mapReviver),
+  };
+  return game;
 };
 
 const loadGameList = (): string[] => {
